264206d562ee9f373411911b152f1e2c9b35ec6f
thuccv98 <thuccv98@gmail.com>
2021-05-19 11:54:58 +0700

    practice todo-list with hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'anh yeu em nhieu lam'},
+    { id: 2, title: 'em yeu anh nhieu lam'},
+    { id: 3, title: 'chung ta chia tay nhe'}
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
       <h1>Welcome to React Hooks!</h1>
 
       <ColorBox />
+      <h1>TodoList</h1>
+
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+TodoList.propTypes = {
+  todo: PropTypes.array,
+  onTodoClick: PropTypes.func
+};
+TodoList.defaultProps = {
+  todos: [],
+  onTodoClick: null
+};
+
+function TodoList(props) {
+  const { todos, onTodoClick } = props;
+
+  function handleClick(todo) {
+    if (onTodoClick) {
+      onTodoClick(todo);
+    }
+  }
+
+  return (
+    <ul className="todo-list">
+      {todos.map(todo => (
+        <li 
+          key={todo.id} 
+          onClick={() => handleClick(todo)}
+        >
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
