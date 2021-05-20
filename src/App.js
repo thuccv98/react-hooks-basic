0ae205d689cf9958d98200a9330c5eca8ba8ed24
thuccv98 <thuccv98@gmail.com>
2021-05-20 10:45:14 +0700

    Practice todo list with hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import ColorBox from './components/ColorBox';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 import { useState } from 'react';
 
 function App() {
@@ -20,6 +21,18 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  function handleTodoFormSubmit(formValues) {
+    console.log('form submit:', formValues); //kiem tra xem du lieu da nhan duoc chua
+    //add new todo to curent todo list
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
       <h1>Welcome to React Hooks!</h1>
@@ -27,6 +40,7 @@ function App() {
       <ColorBox />
       <h1>TodoList</h1>
 
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
